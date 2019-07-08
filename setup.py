04c5d43e064fbb11e9cb0654720cce1e28dede22
kawakami <44276443+Jnchi@users.noreply.github.com>
2019-07-08 14:46:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
     description='Tool for refreshing pages during tests because I am incredibly lazy',
     author='neverrend',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'automouse=automouse:main'
+        ]
+    },
     zip_safe=False
 )
